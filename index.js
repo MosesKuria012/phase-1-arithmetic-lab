5e2d3d54f05c8381d59eae13c79146caa2e49ec7
Moses <t.content012@gmail.com>
2023-03-14 16:05:56 +0300

    math

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
 // Write your code here
+
+// function multiply (num1, num2){
+//     return num1*num2
+// }
+// console.log(multiply(31,2))
+
+const num1 = 31
+const num2 = 2
+
+const multiply = num1*num2
+
+const random = Math.floor(Math.random() * 10) +1
+
+const num3 = 10
+const num4 =  6
+
+const mod = num3%num4
+
+
+const numbers = [20,6,4,4,6]
+    const max = Math.max(...numbers)
